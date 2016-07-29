23b466473da812f0333eb3d92ea8fb73de78a1a8
Голендухин Денис < >
2016-07-30 02:26:19 +0400

    скрипты начало

---
+++ Parallel.World/Assets/Scripts/EventStates.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+public class EventStates
+{
+    private static EventStates _instance;
+    private Dictionary<CustomEventTypes, bool> _eventStates;
+    public static EventStates Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = new EventStates();
+            return _instance;
+        }
+    }
+
+    private EventStates()
+    {
+        GetStatesFromDb();
+    }
+
+    private void GetStatesFromDb()
+    {
+        _eventStates = new Dictionary<CustomEventTypes, bool>();
+        //тут должна быть загрузка из базы, или просто начальные состояня
+        foreach (var type in Enum.GetValues(typeof(CustomEventTypes)))
+        {
+            _eventStates.Add((CustomEventTypes)type, false);
+        }
+    }
+
+    public bool GetState(CustomEventTypes type)
+    {
+        var state = false;
+        return _eventStates.TryGetValue(type, out state) ? false : state;
+    }
+
+    public void ChangeState(CustomEventTypes type, bool state)
+    {
+        _eventStates[type] = state;
+    }
+
+    public void InvertState(CustomEventTypes type)
+    {
+        _eventStates[type] = !_eventStates[type];
+    }
+}
+
+public enum CustomEventTypes
+{
+    Movement = 0,
+    Blablabla = 1,
+    Satan = 2
+}
---
+++ Parallel.World/Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    public float metan;
+    private Vector2 input;
+    private float maxSpeed = 2.2f;
+    void Start()
+    {
+    }
+    void FixedUpdate()
+    {
+        if (EventStates.Instance.GetState(CustomEventTypes.Movement))
+        {
+            input = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+            var rigidbody = GetComponent<Rigidbody2D>();
+            if (rigidbody.velocity.magnitude < maxSpeed)
+            {
+                rigidbody.AddForce(input * metan);
+            }
+            var direction = input.x > 0 ? 1 : -1;
+            transform.localScale = new Vector3(direction, 1, 1);
+        }
+    }
+}
